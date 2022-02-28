05252ca932938ab0cc329f0db87edd8e71f4e593
mvksu <andrzejczakmaks@icloud.com>
2022-02-28 23:51:45 +0100

    header navbar about

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import images from "../../constants/images"
+import React from "react";
+import images from "../../constants/images";
+import { HiMenuAlt4, HiX } from "react-icons/hi";
+import { motion } from "framer-motion";
+import "./Navbar.scss";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li key={`link=${item}`} className="app__flex p-text">
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about", "contact", "work", "skills"].map((item) => (
+                <li key={item} onClick={() => setToggle(false)}>
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
--- src/container/About/About.jsx
+++ src/container/About/About.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
-// import '../About/About.scss'
+import React from "react";
+import "./About.scss";
+import { motion } from "framer-motion";
+import images from "../../constants/images";
+
+const abouts = [
+  {
+    title: "Web Development",
+    description: "I am a goood web developer",
+    imgUrl: images.about01,
+  },
+  {
+    title: "UI/UX",
+    description: "I am a goood web developer",
+    imgUrl: images.about02,
+  },
+  {
+    title: "Backend",
+    description: "I am a goood web developer",
+    imgUrl: images.about03,
+  },
+  {
+    title: "Frontend",
+    description: "I am a goood web developer",
+    imgUrl: images.about04,
+  },
+];
 
 export default function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h2 className="head-text">
+        I know that 
+        <span> Good Design</span>
+        <br />
+        means
+        <span> Good Business</span>
+      </h2>
+
+      <div className="app__profiles">
+        {abouts.map((about, index) => (
+          <motion.div
+            whileInView={{ opacity: 1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: "tween" }}
+            className="app__profile-item"
+            key={about.title + index}
+          >
+            <img src={about.imgUrl} alt={about.title} />
+            <h2 className="bold-text" style={{ marginTop: "20px"}}>{about.title}</h2>
+            <p className="p-text" style={{ marginTop: "10px"}}>{about.description}</p>
+          </motion.div>
+        ))}
+      </div>
+    </div>
+  );
 }
