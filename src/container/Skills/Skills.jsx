798ad0b15e5c93d7bed7a987990dd6a06cb78f57
Maksymilian Andrzejczak <andrzejczakmaks@icloud.com>
2022-03-06 16:37:57 +0100

    work

--- src/container/Skills/Skills.jsx
+++ src/container/Skills/Skills.jsx
@@ -1,5 +1,8 @@
-import React from 'react'
 import './Skills.scss'
+import React, { useState, useEffect } from 'react'
+import { motion } from "framer-motion"
+import AppWrap from "../../wrapper/AppWrap"
+import { client, urlFor } from '../../client'
 
 export default function Skills() {
   return (
